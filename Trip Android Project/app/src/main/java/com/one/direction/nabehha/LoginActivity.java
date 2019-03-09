6956b441d974078a13660f369408f8d973c44149
AbdoAmin <abdelrahman.amin01@gmail.com>
2019-03-09 17:24:04 +0200

    prefrence

--- Trip Android Project/app/src/main/java/com/one/direction/nabehha/LoginActivity.java
+++ Trip Android Project/app/src/main/java/com/one/direction/nabehha/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.one.direction.nabehha;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,10 +14,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.one.direction.nabehha.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener {
@@ -24,24 +25,60 @@ public class LoginActivity extends AppCompatActivity implements
     private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 007;
 
-    SignInButton googleBtn;
+    private ActivityLoginBinding mActivityLoginBinding;
+    private AppExecutors mAppExecutors;
+    private UserPreferencesHelper userPreferencesHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        googleBtn = findViewById(R.id.sign_in_google_button);
-        googleBtn.setOnClickListener(new View.OnClickListener() {
+        mActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        userPreferencesHelper = new AppPreferencesHelper(this, getString(R.string.user_info));
+        mAppExecutors = AppExecutors.getInstance();
+        mActivityLoginBinding.signInGoogleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                loadingUi(true);
                 GoogleSignIn();
             }
         });
+        mActivityLoginBinding.emailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadingUi(true);
+                mAppExecutors.networkIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        //TODO WS LogIn
+
+//                        User user = WS.login(mActivityLoginBinding.email.getText().toString()
+//                                , mActivityLoginBinding.password.getText().toString());
+
+
+                        mAppExecutors.mainThread().execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                loadingUi(false);
+//                                if (user.getEmail() != null) {
+//                                    passUser(user);
+//                                } else {
+//                                    Toast.makeText(LoginActivity.this,
+//                                            "You Are Not Registered or wrong password",
+//                                            Toast.LENGTH_SHORT).show();
+//                                }
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
     }
 
 
     private void GoogleSignIn() {
-        if(mGoogleApiClient==null) {
+        if (mGoogleApiClient == null) {
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestEmail()
                     .build();
@@ -82,21 +119,18 @@ public class LoginActivity extends AppCompatActivity implements
             GoogleSignInAccount acct = result.getSignInAccount();
 
             if (acct != null) {
-                Log.e(TAG, "display name: " + acct.getDisplayName());
-
                 String personName = acct.getDisplayName();
                 String personPhotoUrl = acct.getPhotoUrl().toString();
                 String email = acct.getEmail();
 
-
-                Log.e(TAG, "Name: " + personName + ", email: " + email
-                        + ", Image: " + personPhotoUrl);
-
-                Intent profile = new Intent(this, MainActivity.class);
+                Intent profile = new Intent(this, SplashActivity.class);
 //            profile.putExtra(getString(R.string.user_name), personName);
 //            profile.putExtra(getString(R.string.user_email), email);
 //            profile.putExtra(getString(R.string.user_photo), personPhotoUrl);
                 startActivity(profile);
+
+                //TODO WS LogIn
+                //if not exist : Register
             }
 
 
@@ -105,33 +139,52 @@ public class LoginActivity extends AppCompatActivity implements
         }
     }
 
+    private void loadingUi(boolean isLoading) {
+        if (isLoading) {
+            mActivityLoginBinding.loginProgress.setVisibility(View.VISIBLE);
+        } else {
+            mActivityLoginBinding.loginProgress.setVisibility(View.GONE);
+        }
+        mActivityLoginBinding.emailSignInButton.setEnabled(!isLoading);
+        mActivityLoginBinding.signInGoogleButton.setEnabled(!isLoading);
+    }
+
+    private void passUser(/*User user*/) {
+        /*
+        userPreferencesHelper.setCurrentUserId(user.getId());
+        userPreferencesHelper.setCurrentUserEmail(user.getEmail);
+        userPreferencesHelper.setCurrentUserName(user.getName);
+         //TODO Go To Main Screen
+        */
+    }
+
 
     @Override
     public void onStart() {
         super.onStart();
-        if (mGoogleApiClient != null) {
-            OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-            if (opr.isDone()) {
-                // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-                // and the GoogleSignInResult will be available instantly.
-                Log.e(TAG, "Got cached sign-in");
-                GoogleSignInResult result = opr.get();
-                handleSignInResult(result);
-            } else {
-                // If the user has not previously signed in on this device or the sign-in has expired,
-                // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-                // single sign-on will occur in this branch.
-//            showProgressDialog();
-                opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                    @Override
-                    public void onResult(GoogleSignInResult googleSignInResult) {
-//                    hideProgressDialog();
-                        Log.e(TAG, "Gotn't cached sign-in");
-                        handleSignInResult(googleSignInResult);
-                    }
-                });
-            }
-        }
+//        if (mGoogleApiClient != null) {
+//            OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+//            if (opr.isDone()) {
+//                // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+//                // and the GoogleSignInResult will be available instantly.
+//                Log.e(TAG, "Got cached sign-in");
+//                GoogleSignInResult result = opr.get();
+//                handleSignInResult(result);
+//            } else {
+//                // If the user has not previously signed in on this device or the sign-in has expired,
+//                // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+//                // single sign-on will occur in this branch.
+////            showProgressDialog();
+//                opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+//                    @Override
+//                    public void onResult(GoogleSignInResult googleSignInResult) {
+////                    hideProgressDialog();
+//                        Log.e(TAG, "Gotn't cached sign-in");
+//                        handleSignInResult(googleSignInResult);
+//                    }
+//                });
+//            }
+//        }
     }
 
 }
