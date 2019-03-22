5f5cb55fae29a4fd849c8676cc286c19952602a0
AbdoAmin <abdelrahman.amin01@gmail.com>
2019-03-23 01:19:50 +0200

    splash screen

--- Trip Android Project/app/src/main/java/com/one/direction/nabehha/SplashActivity.java
+++ Trip Android Project/app/src/main/java/com/one/direction/nabehha/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Build;
+import android.os.CountDownTimer;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,23 +13,15 @@ import android.widget.Toast;
 import com.one.direction.nabehha.databinding.ActivitySplashScreenBinding;
 
 public class SplashActivity extends AppCompatActivity {
+    CountDownTimer countDownTimer;
 
-    private ActivitySplashScreenBinding mActivitySplashBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mActivitySplashBinding=DataBindingUtil.setContentView(this, R.layout.activity_splash_screen);
-        UserPreferencesHelper userPreferencesHelper=new AppPreferencesHelper(this,getString(R.string.user_info));
+        DataBindingUtil.setContentView(this, R.layout.activity_splash_screen);
+        final UserPreferencesHelper userPreferencesHelper=new AppPreferencesHelper(this,getString(R.string.user_info));
         AppConstants.CURRENT_USER_EMAIL=userPreferencesHelper.getCurrentUserEmail();
-        if(AppConstants.CURRENT_USER_EMAIL!=null){
-            AppConstants.CURRENT_USER_NAME=userPreferencesHelper.getCurrentUserName();
-            AppConstants.CURRENT_USER_ID=userPreferencesHelper.getCurrentUserId();
-            //TODO Go To Main Screen
 
-        }
-        else{
-            startActivity(new Intent(this,SignUpActivity.class));
-        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
             //If the draw over permission is not available open the settings screen
             //to grant the permission.
@@ -37,6 +30,27 @@ public class SplashActivity extends AppCompatActivity {
             startActivityForResult(intent, 100);
         }
         //finish();
+        countDownTimer=new CountDownTimer(1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+            @Override
+            public void onFinish() {
+                if(AppConstants.CURRENT_USER_EMAIL!=null){
+                    AppConstants.CURRENT_USER_NAME=userPreferencesHelper.getCurrentUserName();
+                    AppConstants.CURRENT_USER_ID=userPreferencesHelper.getCurrentUserId();
+                    //TODO Go To Main Screen
+                    startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                }
+                else{
+                    startActivity(new Intent(SplashActivity.this,SignUpActivity.class));
+                }
+                finish();
+            }
+
+        };
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -54,5 +68,23 @@ public class SplashActivity extends AppCompatActivity {
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+    @Override
+    protected void onResume() {
+        countDownTimer.start();
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            //If the draw over permission is not available open the settings screen
+            //to grant the permission.
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, 100);
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        countDownTimer.cancel();
     }
 }
